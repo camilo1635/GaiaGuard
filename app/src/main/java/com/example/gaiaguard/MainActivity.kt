26ec3d76458e1927f6d13a895e8eecc2e697f0f4
camilo1635 <jcorreac@unicauca.edu.co>
2024-10-05 12:24:10 -0500

    imagenes iconos

--- app/src/main/java/com/example/gaiaguard/MainActivity.kt
+++ app/src/main/java/com/example/gaiaguard/MainActivity.kt
@@ -19,16 +19,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GaiaGuardTheme {
-                GaiaGuardApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GaiaGuardTheme {
-        GaiaGuardApp()
+        Greeting("Android")
     }
 }
