a4c77580a35d92f8615b5e2057eaae40496e6054
camilo1635 <jcorreac@unicauca.edu.co>
2024-10-05 16:10:05 -0500

    cambio de info

--- app/src/main/java/com/example/gaiaguard/ui/screen/menu/Menu.kt
+++ app/src/main/java/com/example/gaiaguard/ui/screen/menu/Menu.kt
@@ -65,3 +65,18 @@ fun MyOptionsList(
         }
     }
 }
+
+@Composable
+fun MyOptionsListApp() {
+    val options = listOf("1. Fin de la pobreza", "2. Hambre cero", "3. Salud y bienestar", "4. Educación de calidad",
+        "5. Igualdad de género", "6. Agua limpia y saneamiento", "7. Enegía asequible y no contaminante", "8. Trabajo decente y cremiento económico",
+        "9. Industria, innovación e infraestructura", "10. Reducción de las desigualdades", "11. Ciudades y comunidades sostenibles", "12. Producción y consumo responsables",
+        "13. Agua por el clima", "14. Vida Submarina","15. Vida de ecosistemas","16.Paz, justicia e instituciones solidarias", "17. Alianzas para lograr los objetivos")
+    var selectedOptions by remember { mutableStateOf(emptySet<String>()) }
+
+    MyOptionsList(
+        options = options,
+        selectedOptions = selectedOptions,
+        onOptionSelected = { selectedOptions = it }
+    )
+}
